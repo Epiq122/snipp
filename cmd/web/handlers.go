4d54f40da507363a3e9a09cef993a258c4637aa9
Robert Gleason <l2ob122@gmail.com>
2025-08-18 20:16:17 -0700

    Refactor project structure: move `main.go` to `cmd/web`, reorganize handlers.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -30,18 +29,4 @@ func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("Saving new snippet"))
 
-}
-
-func main() {
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
-
-	log.Print("Server started on port 8080")
-
-	err := http.ListenAndServe(":8080", mux)
-	log.Fatal(err)
 }
