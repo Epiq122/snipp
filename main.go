02dcc97de6e089042948b6e9bd1354ef1ca7a290
Robert Gleason <l2ob122@gmail.com>
2025-08-18 19:48:13 -0700

    Add snippet routes and Git Toolbox configuration

--- main.go
+++ main.go
@@ -9,10 +9,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from the snipp"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet"))
+}
+
 func main() {
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Server started on port 8080")
 
