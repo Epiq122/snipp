2b1ebfca4f7f1d7132b668c06030c87fd2846010
Robert Gleason <l2ob122@gmail.com>
2025-08-19 21:41:15 -0700

    feat: implement template caching and rendering logic; refactor handlers to utilize new render method

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // The serverError helper writes a log entry at Error level (including the request
 // method and URI as attributes), then sends a generic 500 Internal Server Error
@@ -21,4 +24,24 @@ func (app *application) serverError(w http.ResponseWriter, r *http.Request, err 
 
 func (app *application) clientError(w http.ResponseWriter, status int) {
 	http.Error(w, http.StatusText(status), status)
+}
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, status int, page string, data templateData) {
+	// Retrieve the appropriate template set from the cache based on the page
+	// name (like 'home.tmpl').
+	ts, ok := app.templateCache[page]
+	if !ok {
+		err := fmt.Errorf("template %s not found", page)
+
+		app.serverError(w, r, err)
+		return
+	}
+
+	w.WriteHeader(status)
+
+	err := ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
 }
