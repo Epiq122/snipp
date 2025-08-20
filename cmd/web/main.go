afeda347b09618c7fa2abfce6349adf5c505d8b4
Robert Gleason <l2ob122@gmail.com>
2025-08-19 18:05:30 -0700

    Switch to slog package for structured logging in main.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -14,6 +15,10 @@ func main() {
 
 	mux := http.NewServeMux()
 
+	// Use the slog.New() function to initialize a new structured logger, which
+	// writes to the standard out stream and uses the default settings.
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
@@ -22,8 +27,13 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Printf("starting on server %s", *addr)
+	logger.Info("starting on server %s", *addr)
 
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+
+	// And we also use the Error() method to log any error message returned by
+	// http.ListenAndServe() at Error severity (with no additional attributes),
+	// and then call os.Exit(1) to terminate the application with exit code 1.
+	logger.Error(err.Error())
+	os.Exit(1)
 }
